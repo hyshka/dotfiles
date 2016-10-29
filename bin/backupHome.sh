ffe4456ea5ed4d36cd451d12ccfde1a201207beb
Bryan Hyshka <bryan@hyshka.com>
2016-10-29 16:08:01 -0600

    update backup home script, and add pkglist backup

--- bin/backupHome.sh
+++ bin/backupHome.sh
@@ -1,30 +1,29 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 if [ "$HOSTNAME" = brix ]; then
-	DEST="/run/media/hyshka/Lift Portable/brix-home"
+	DESTINATION="/run/media/hyshka/Lift Portable/brix-home"
 else
-	DEST=/media/Portable/hyshka/
+	DESTINATION=/media/Portable/hyshka/
 fi
 
-SRC=/home/hyshka
+SOURCE=/home/hyshka
 
-rsync \
-	# -a handles files in archive mode
-	# -A preserve ACLs
-	# -X preserve extended attributes
-	# -S handle sparse files efficiently
-	# -v increase verbosity
+# -a handles files in archive mode
+# -A preserve ACLs
+# -X preserve extended attributes
+# -S handle sparse files efficiently
+# -v increase verbosity
+# --delete deletes extraneous files from destination dirs
+# --info=progress2 show progress bars
+# do each exclude separately
+sudo rsync \
 	-aAXSv \
-	# delete extraneous files from dest dirs
 	--delete \
-	# show progress bars
 	--info=progress2 \
-	# do each exclude separately
-	--exclude="$SRC/.thumbnails" \
-	--exclude="$SRC/.cache" \
-	--exclude="$SRC/.local/share/Trash" \
-	--exclude="$SRC/.gvfs" \
-	--exclude="$SRC/tmp" \
-	$SRC \ # source
-	$DEST # destination
-
+	--exclude="$SOURCE/.thumbnails/*" \
+	--exclude="$SOURCE/.cache/*" \
+	--exclude="$SOURCE/.local/share/Trash/*" \
+	--exclude="$SOURCE/.gvfs/*" \
+	--exclude="$SOURCE/tmp/*" \
+	$SOURCE \
+	$DESTINATION
