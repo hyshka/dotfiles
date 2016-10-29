d03e6ec1752c79dc9134ecaaf6ebeb8b30da8485
Bryan Hyshka <bryan@hyshka.com>
2016-10-29 16:15:09 -0600

    simplify root backup

--- bin/backupRoot.sh
+++ bin/backupRoot.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/sh
 # Requires dar and par2cmdline
 
 if [ "$HOSTNAME" = brix ]; then
-	sudo dar -B ~/.bin/darRootBrix --create "/run/media/hyshka/Lift Portab/brix-root/$(date +%F)-brix-root"
+	sudo dar -B ~/.bin/darRoot --create "/run/media/hyshka/Lift Portab/brix-root/$(date +%F)-brix-root"
 	sudo chown -R hyshka:hyshka "/run/media/hyshka/Lift Portab/brix-root"
 else
 	sudo dar -B ~/.bin/darRoot --create /media/Portable/w520-root/$(date +%F)-w520-root
