6a43c7781947290ed7efa81f8946fa2bcf7a45a0
Bryan Hyshka <bryan@hyshka.com>
2017-08-02 18:53:58 -0600

    update osync setup to latest version

--- bin/backupHome.sh
+++ bin/backupHome.sh
@@ -2,7 +2,7 @@
 # requires osync.sh script
 
 if [ "$HOSTNAME" = brix ]; then
-	sudo osync.sh ~/dotfiles/osync/brix_sync.conf
+	sudo bash osync.sh ~/dotfiles/osync/brix_sync.conf
 else
-	sudo osync.sh ~/dotfiles/osync/w520_sync.conf
+	sudo bash osync.sh ~/dotfiles/osync/w520_sync.conf
 fi
--- bin/backupRoot.sh
+++ bin/backupRoot.sh
@@ -5,6 +5,6 @@ if [ "$HOSTNAME" = brix ]; then
 	sudo dar -B ~/.bin/darRoot --create "/run/media/hyshka/Lift Portab/brix-root/$(date +%F)-brix-root"
 	sudo chown -R hyshka:hyshka "/run/media/hyshka/Lift Portab/brix-root"
 else
-	sudo dar -B ~/.bin/darRoot --create /media/Portable/w520-root/$(date +%F)-w520-root
-	sudo chown -R hyshka:hyshka /media/Portable/w520-root/
+	sudo dar -B ~/.bin/darRoot --create /run/media/hyshka/Portable/w520-root/$(date +%F)-w520-root
+	sudo chown -R hyshka:hyshka /run/media/hyshka/Portable/w520-root/
 fi
