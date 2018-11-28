43326bd7fd7da388c5e5ea3a8ac4d42f54936ba4
Bryan Hyshka <bryan@hyshka.com>
2018-11-28 12:36:33 -0700

    remove unused scripts

--- bin/colors_bg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for C in {0..255}; do
-    tput setaf $C
-    echo -n "$C "
-done
-tput sgr0
-echo
--- bin/setupPip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-sudo pip install virtualenv virtualenvwrapper virtualenv-clone cookiecutter
-sudo pip2 install virtualenv virtualenvwrapper virtualenv-clone cookiecutter
