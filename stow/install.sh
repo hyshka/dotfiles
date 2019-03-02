e3f41cb5c52a7e4eb9a63d1a51aef14f9bac4986
Bryan Hyshka <bryan@hyshka.com>
2019-03-01 17:54:18 -0700

    Add host specific i3 stuff

--- stow/install.sh
+++ stow/install.sh
@@ -3,12 +3,13 @@ for APP in ${APPS[@]}; do
     stow "$APP" -t "$HOME"
 done
 
-# i3
-# if [ "$HOSTNAME" = veyron ]; then
-#   j4-make-config -a config_veyron solarized_light
-#   ln -s ~/dotfiles/i3/i3status_veyron ~/dotfiles/i3/i3status.conf
-# fi
-# if [ "$HOSTNAME" = gandof ]; then
-#   j4-make-config -a config_gandof solarized_light
-#   ln -s ~/dotfiles/i3/i3status_gandof ~/dotfiles/i3/i3status.conf
-# fi
+# i3 host-specifics
+if [ "$HOSTNAME" = veyron ]; then
+  j4-make-config -a config_veyron solarized_light
+  ln -s ~/dotfiles/i3/i3status_veyron ~/.config/i3/i3status.conf
+fi
+
+if [ "$HOSTNAME" = gandof ]; then
+  j4-make-config -a config_gandof solarized_light
+  ln -s ~/dotfiles/i3/i3status_gandof ~/.config/i3/i3status.conf
+fi
