64390745036736caeff2d008de41e1ae0be52957
Bryan Hyshka <bryan@hyshka.com>
2021-04-21 13:24:40 -0600

    Add xidlehook, update port forwards

--- bin/pyclip.py
+++ bin/pyclip.py
@@ -5,6 +5,7 @@ import time
 
 import pyperclip
 
+
 def receive_all(socket: socket.socket):
     """We're assuming small payloads of text.
 
@@ -16,9 +17,10 @@ def receive_all(socket: socket.socket):
     socket.close()
     return data
 
+
 def parse_data(data: bytes):
     """Parse data sent to socket."""
-    data_str = data.decode('utf-8').strip()
+    data_str = data.decode("utf-8").strip()
     num_lines = len(data_str.splitlines())
     # Check if it was a docker port passed back
     print(data_str)
@@ -28,16 +30,25 @@ def parse_data(data: bytes):
         except ValueError:
             pass
         else:
-            if 1000 < int_input < 40000:
-                print('Opening Docker port: {}'.format(int_input))
+            if 1000 < int_input < 50000:
+                print("Opening Docker port: {}".format(int_input))
                 proc = subprocess.Popen(
-                    ['ssh', '-nNT', '-L', '{}:localhost:{}'.format(int_input, int_input), 'dev'],
-                    stdout=subprocess.PIPE
+                    [
+                        "ssh",
+                        "-nNT",
+                        "-L",
+                        "{}:localhost:{}".format(int_input, int_input),
+                        "dev",
+                    ],
+                    stdout=subprocess.PIPE,
                 )
                 time.sleep(0.5)  # Wait for SSH to connect
                 proc = subprocess.Popen(
-                    ['firefox-developer-edition', 'http://localhost:{}'.format(int_input)],
-                    stdout=subprocess.PIPE
+                    [
+                        "firefox-developer-edition",
+                        "http://localhost:{}".format(int_input),
+                    ],
+                    stdout=subprocess.PIPE,
                 )
                 return
     pyperclip.copy(data_str)
@@ -45,25 +56,25 @@ def parse_data(data: bytes):
 
 def listen_for_data(sock: socket.socket) -> None:
     """Make the socket listen for data forever."""
-    host = 'localhost'
+    host = "localhost"
     port = 41401
     sock.bind((host, port))
     sock.listen(1)
     while True:
-        print('Waiting...')
+        print("Waiting...")
         conn, addr = sock.accept()
-        print(f'Connection from {addr}')
-        if addr[0] != '127.0.0.1':
+        print(f"Connection from {addr}")
+        if addr[0] != "127.0.0.1":
             continue
         with conn:
             data = receive_all(conn)
             parse_data(data)
 
 
-if __name__ == '__main__':
-    print('Attempting to forward SSH port')
+if __name__ == "__main__":
+    print("Attempting to forward SSH port")
     subprocess.Popen(
-        ['ssh', '-nNT', '-R', '41401:localhost:41401', 'dev'],
+        ["ssh", "-nNT", "-R", "41401:localhost:41401", "dev"],
         stdout=subprocess.PIPE,
     )
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
